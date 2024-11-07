f87bcba81ebe37fc19c5bc76a2a6fa7f1d9c3358
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2024-11-07 14:41:38 +0900

    Minor update.

--- ogura.py
+++ ogura.py
@@ -2,21 +2,16 @@ import numpy as np
 import numpy.typing as npt
 
 
-def solve(
-    image: npt.NDArray[np.uint8],
-    poems: list[str],
-    level: int,
-) -> list[tuple[int, str]]:
+def solve(image: npt.NDArray[np.uint8], poems: list[str], level: int) -> list[int]:
     """
     Inputs:
       image: input color image with size (H, W, 3)
       poems: list of Ogura poems
       level: difficulty of the problem (from 1 to 3)
     Outputs:
-      answer: list of tuples of integer and string, where integer represent
-      the number with which the player can guess the poem, while the string
-      represent the poem itself. If the poem is inexistent, the integer should
-      be 0, and the string should be an empty one.
+      answer: list of integers, where each integer represent the number
+      with which the player can uniquely determine the corresponding poem.
+      If the poem is inexistent, the integer is expected to be 0.
     """
-    answer = [(0, "")] * len(poems)
+    answer = [0] * len(poems)
     return answer
